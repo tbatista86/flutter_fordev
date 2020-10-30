2975661ac26d17d77f31b445d481f73c187dec48
Tiago Batista <tiagobatista86@gmail.com>
2020-10-30 09:11:05 -0300

    ensure ValidationComposite returns the first error found

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -10,7 +10,12 @@ class ValidationComposite implements Validation {
   ValidationComposite(this.validations);
 
   String validate({@required String field, @required String value}) {
-    return null;
+    String error;
+    for (final validation in validations) {
+      error = validation.validate(value);
+      if (error?.isNotEmpty == true) return error;
+    }
+    return error;
   }
 }
 
@@ -36,7 +41,7 @@ void main() {
 
   setUp(() {
     validation1 = FieldValidationSpy();
-    when(validation1.field).thenReturn('any_field');
+    when(validation1.field).thenReturn('other_field');
     mockValidation1(null);
     validation2 = FieldValidationSpy();
     when(validation2.field).thenReturn('any_field');
@@ -48,7 +53,19 @@ void main() {
   });
   test('Should return null if all validations returns null or empty', () {
     mockValidation2('');
+
     final error = sut.validate(field: 'any_field', value: 'any_value');
+
     expect(error, null);
+  });
+
+  test('Should return null if all validations returns null or empty', () {
+    mockValidation1('error_1');
+    mockValidation2('error_2');
+    mockValidation3('error_3');
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, 'error_2');
   });
 }
