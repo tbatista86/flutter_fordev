ef5d68480615af5598b935561e805f2241708a75
Tiago Batista <tiagobatista86@gmail.com>
2020-12-29 09:10:31 -0300

    move RemoteloadSurveys to its own file

---
+++ lib/data/usecases/load_surveys/remote_load_surveys.dart
@@ -0,0 +1,29 @@
+import 'package:meta/meta.dart';
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../http/http.dart';
+import '../../models/remote_survey_model.dart';
+
+class RemoteLoadSurveys implements LoadSurveys {
+  final String url;
+  final HttpClient<List<Map>> httpClient;
+
+  RemoteLoadSurveys({
+    @required this.url,
+    @required this.httpClient,
+  });
+
+  Future<List<SurveyEntity>> load() async {
+    try {
+      final httpResponse = await httpClient.request(url: url, method: 'get');
+      return httpResponse
+          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
+          .toList();
+    } on HttpError catch (error) {
+      throw error == HttpError.forbidden
+          ? DomainError.accessDenied
+          : DomainError.unexpected;
+    }
+  }
+}
--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -1,35 +1,10 @@
-import 'package:meta/meta.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-
 import 'package:fordev/domain/helpers/helpers.dart';
-import 'package:fordev/data/models/models.dart';
 import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/data/http/http.dart';
-
-class RemoteLoadSurveys {
-  final String url;
-  final HttpClient<List<Map>> httpClient;
-
-  RemoteLoadSurveys({
-    @required this.url,
-    @required this.httpClient,
-  });
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final httpResponse = await httpClient.request(url: url, method: 'get');
-      return httpResponse
-          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
-          .toList();
-    } on HttpError catch (error) {
-      throw error == HttpError.forbidden
-          ? DomainError.accessDenied
-          : DomainError.unexpected;
-    }
-  }
-}
+import 'package:fordev/data/usecases/usecases.dart';
 
 class HttpClientSpy extends Mock implements HttpClient<List<Map>> {}
 
