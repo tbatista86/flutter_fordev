9deea116a7c6c7713c2e1b151e8f688f40e36529
Tiago Batista <tiagobatista86@gmail.com>
2021-01-04 11:50:52 -0300

    move sut creation to the setup method

---
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -0,0 +1,34 @@
+import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
+import 'package:test/test.dart';
+import 'package:fordev/data/cache/cache.dart';
+
+class AuthorizeHttpClientDecorator {
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  AuthorizeHttpClientDecorator({@required this.fetchSecureCacheStorage});
+
+  Future<void> request() async {
+    await fetchSecureCacheStorage.fetchSecure('token');
+  }
+}
+
+class FetchSecureCacheStorageSpy extends Mock
+    implements FetchSecureCacheStorage {}
+
+void main() {
+  FetchSecureCacheStorageSpy fetchSecureCacheStorage;
+  AuthorizeHttpClientDecorator sut;
+
+  setUp(() {
+    fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    sut = AuthorizeHttpClientDecorator(
+        fetchSecureCacheStorage: fetchSecureCacheStorage);
+  });
+
+  test('Should call FetchSecureCacheStorage with correct key', () async {
+    await sut.request();
+
+    verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+}
