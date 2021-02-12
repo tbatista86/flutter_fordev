d5747d9f3d5b15a764100fd67ce0eeef4aa0dd15
Tiago Batista <tiagobatista86@gmail.com>
2021-02-12 08:52:23 -0300

    rename localsotorageadapter to securestorageadpters

--- lib/main/factories/http/authorize_http_client_decorator_factory.dart
+++ lib/main/factories/http/authorize_http_client_decorator_factory.dart
@@ -4,4 +4,4 @@ import '../../decorators/decorators.dart';
 
 HttpClient makeAuthorizeHtppClientDecorator() => AuthorizeHttpClientDecorator(
     decoratee: makeHttpAdapter(),
-    fetchSecureCacheStorage: makeLocalStorageAdapter());
+    fetchSecureCacheStorage: makeSecureStorageAdapter());
