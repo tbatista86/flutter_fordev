7b1adf27d0e7d161df782a1e683201bb5ce36f50
Tiago Batista <tiagobatista86@gmail.com>
2021-02-12 10:47:34 -0300

    ensure Composite calls remote load

---
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -0,0 +1,27 @@
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
+import 'package:fordev/data/usecases/load_surveys/load_surveys.dart';
+
+class RemoteLoadSurveysWithLocalFallback {
+  final RemoteLoadSurveys remote;
+
+  RemoteLoadSurveysWithLocalFallback({@required this.remote});
+
+  Future<void> load() async {
+    await remote.load();
+  }
+}
+
+class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
+
+void main() {
+  test('Should call remote load', () async {
+    final remote = RemoteLoadSurveysSpy();
+    final sut = RemoteLoadSurveysWithLocalFallback(remote: remote);
+
+    await sut.load();
+
+    verify(remote.load()).called(1);
+  });
+}
