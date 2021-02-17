c928a4de6bf84f19a7940ab06dae753ff78a388a
Tiago Batista <tiagobatista86@gmail.com>
2021-02-17 10:05:38 -0300

    error remote_survey_result_model.dart

--- lib/domain/entities/survey_result_entity.dart
+++ lib/domain/entities/survey_result_entity.dart
@@ -1,14 +1,17 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import './entities.dart';
 
-class SurveyResultEntity {
-  final String suurveyId;
+class SurveyResultEntity extends Equatable {
+  final String surveyId;
   final String question;
-  final List<SurveyAnswerEntity> didAnswer;
+  final List<SurveyAnswerEntity> answers;
+
+  List get props => ['surveyId', 'question', 'answers'];
 
   SurveyResultEntity({
-    @required this.suurveyId,
+    @required this.surveyId,
     @required this.question,
-    @required this.didAnswer,
+    @required this.answers,
   });
 }
