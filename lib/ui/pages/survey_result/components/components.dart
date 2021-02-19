3907ee3650a0215da5ff801f74dcf8dbfd226071
Tiago Batista <tiagobatista86@gmail.com>
2021-02-19 14:57:32 -0300

    move SurveyAnswer to ites onw file and refact the same file

--- lib/ui/pages/survey_result/components/components.dart
+++ lib/ui/pages/survey_result/components/components.dart
@@ -2,3 +2,4 @@ export './survey_result.dart';
 export './active_icon.dart';
 export './disabled_icon.dart';
 export './survey_header.dart';
+export './survey_answer.dart';
--- lib/ui/pages/survey_result/components/survey_result.dart
+++ lib/ui/pages/survey_result/components/survey_result.dart
@@ -14,48 +14,7 @@ class SurveyResult extends StatelessWidget {
         if (index == 0) {
           return SurveyHeader(viewModel.question);
         }
-        return Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Theme.of(context).backgroundColor,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  viewModel.answers[index - 1].image != null
-                      ? Image.network(
-                          viewModel.answers[index - 1].image,
-                          width: 40,
-                        )
-                      : SizedBox(height: 0),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        viewModel.answers[index - 1].answer,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    viewModel.answers[index - 1].percent,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).primaryColorDark,
-                    ),
-                  ),
-                  viewModel.answers[index - 1].isCurrentAnswer
-                      ? ActiveIcon()
-                      : DisabledIcon()
-                ],
-              ),
-            ),
-            Divider(height: 1),
-          ],
-        );
+        return SurveyAnswer(viewModel.answers[index - 1]);
       },
       itemCount: viewModel.answers.length + 1,
     );
